1535ac74784bbd895125fb1075710325b502a224
Eyleen Krikk <113108128+eyleenk@users.noreply.github.com>
2024-05-02 15:41:51 +0300

    Delete eyleen_kodutöö 6.1.py

--- eyleen_kodutöö 6.1.py
+++
@@ -1,3 +0,0 @@
-def solution(text, ending): #definereeib fn(solution) mis võtab argumentideks text ja ending
-    return text.endswith(ending) #endswith funktsioon kontrollib, kas sõna(text) lõppeb antud väärtustega(ending), 
-#kui jah, siis väljastab true, kui ei, siis false
